b9a1dbcee28ffc25a32abb415ad4a469a75a13e4
AnanthRka <r.ananth04@gmail.com>
2021-08-19 23:04:14 +0530

    download added

--- graphs.py
+++ graphs.py
@@ -1,4 +1,5 @@
-from datetime import  datetime,date, timedelta
+from datetime import  datetime, timedelta
+import urllib.request
 
 now = datetime.now()
 hour = datetime.time(now).hour
@@ -7,5 +8,7 @@ minute = datetime.time(now).minute
 today_date = datetime.strftime(now, '%d-%m-%y')
 if hour <20 and minute >=30:
     today_date = datetime.strftime(now -timedelta(1), '%d-%m-%y' )
-print (today_date)
+
 url = 'https://stopcorona.tn.gov.in/wp-content/uploads/2020/03/Media-Bulletin-'+ today_date + '-COVID-19.pdf'
+
+urllib.request.urlretrieve(url, filename='data.pdf')
